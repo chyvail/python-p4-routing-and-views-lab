7f423c24e4c3c99f358735d2d440966756779e3c
joshua chivile <joshuachivile@gmail.com>
2024-01-09 17:44:08 +0300

    working with routes and views

--- server/app.py
+++ server/app.py
@@ -4,5 +4,43 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param)
+    return param
+
+@app.route('/count/<int:param>')
+def count(param):
+    result = ""
+    for i in range(param):
+        result += f"{i}\n"
+    return result
+
+@app.route('/math/<num1>/<operation>/<num2>')
+def math(num1,operation,num2):
+    if (operation == "+"):
+        value = int(num1) + int(num2)
+        return str(value)
+    
+    elif (operation == "-"):
+        value = int(num1) - int(num2)
+        return str(value)
+    
+    elif (operation == "*"):
+        value = int(num1) * int(num2)
+        return str(value)
+    
+    elif (operation == "div"):
+        value = int(num1) / int(num2)
+        return str(value)
+    
+    elif (operation == "%"):
+        value = int(num1) % int(num2)
+        return str(value)
+    
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
